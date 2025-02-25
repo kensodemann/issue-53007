35fac9a836f6d5abcf90325d20162aa225cafad8
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-02-25 07:15:58 -0600

    make the actual unlock more visible

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import {
   IonButton,
   IonContent,
@@ -13,6 +13,7 @@ import {
 import { SessionVaultService, UnlockMode } from '../core/session-vault.service';
 import { Session } from '../models/session';
 import { AuthenticationService } from '../core/authentication.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -20,17 +21,31 @@ import { AuthenticationService } from '../core/authentication.service';
   styleUrls: ['tab1.page.scss'],
   imports: [IonButton, IonContent, IonHeader, IonItem, IonLabel, IonList, IonTitle, IonToolbar, IonTitle],
 })
-export class Tab1Page implements OnInit {
+export class Tab1Page implements OnInit, OnDestroy {
   session: Session | null = null;
+  lockSubscription: Subscription;
 
   constructor(
     private authentication: AuthenticationService,
     private navController: NavController,
     private sessionVault: SessionVaultService,
-  ) {}
+    ngZone: NgZone,
+  ) {
+    this.lockSubscription = this.sessionVault.locked$.subscribe((locked: boolean) => {
+      if (locked) {
+        this.session = null;
+      } else {
+        ngZone.run(async () => (this.session = await this.sessionVault.getSession()));
+      }
+    });
+  }
 
   async ngOnInit() {
     this.session = await this.sessionVault.getSession();
+  }
+
+  ngOnDestroy(): void {
+    this.lockSubscription.unsubscribe();
   }
 
   async logout(): Promise<void> {
